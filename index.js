8099920882b40e34f9afd8e3dd73a6968e8dece2
avivgalula <avivgalula@gmail.com>
2023-08-11 18:18:23 +0300

    tesing

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 const cors = require("cors");
-const scrapers = require("./scrapers");
 const model = require("./model");
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,25 +10,33 @@ app.use(cors());
 
 app.get("/jobs/alljobs", async (req, res) => {
   const query = req.query.search;
-  const jobs = await model.getAllJobs(query);
-  console.log("jobs");
-  console.log(jobs);
-
-  res.json(jobs);
+  const page = req.query.page;
+  try {
+    const jobs = await model.getAllJobs(query, page);
+    res.json(jobs);
+  } catch (err) {
+    console.error("Error fetching all jobs:", err);
+    res.status(500).json({ err: "An error occurred while fetching the jobs." });
+  }
 });
 
 app.get("/jobs/jobmaster", async (req, res) => {
   const query = req.query.search;
-  const jobs = await model.getJobMaster(query);
-
-  res.json(jobs);
+  const page = req.query.page;
+  try {
+    const jobs = await model.getJobMaster(query, page);
+    res.json(jobs);
+  } catch (err) {
+    console.error("Error fetching all jobs:", err);
+    res.status(500).json({ err: "An error occurred while fetching the jobs." });
+  }
 });
 
 app.get("/jobs/drushim", async (req, res) => {
   const query = req.query.search;
+  const page = req.query.page;
   try {
-    const jobs = await model.getDrushim(query);
-
+    const jobs = await model.getDrushim(query, page);
     res.json(jobs);
   } catch (err) {
     console.error("Error fetching all jobs:", err);
@@ -38,7 +45,7 @@ app.get("/jobs/drushim", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ home: "scraping jobs api" });
+  res.json({ home: "search jobs api" });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- model.js
+++ model.js
@@ -45,192 +45,217 @@ const getDrushim = async function (query, page = 1) {
 };
 
 const getAllJobs = async function (query, page = 1) {
-  const url = `https://www.alljobs.co.il/SearchResultsGuest.aspx?page=${page}&position=&type=&freetxt=${query}&city=&region=`;
-  console.log("Start scraping AllJobs...");
-  const html = await axios(url);
-  const $ = cheerio.load(html.data);
-  const jobPosts = $(".openboard-container-jobs .job-content-top");
-  const jobs_data = [];
+  try {
+    const url = `https://www.alljobs.co.il/SearchResultsGuest.aspx?page=${page}&position=&type=&freetxt=${query}&city=&region=`;
+    console.log("Start scraping AllJobs...");
+    const html = await axios(url);
+    const $ = cheerio.load(html.data);
+    const jobPosts = $(".openboard-container-jobs .job-content-top");
+    const jobs_data = [];
 
-  // Helper
-  const findElement = function (parentEl, queries) {
-    let childEl;
-    let helper;
-    queries.forEach((query) => {
-      if (!helper) {
-        childEl = $(parentEl).find(query);
-        helper = $(parentEl).find(query).text();
-      }
-    });
-    return childEl;
-  };
-
-  jobPosts.each((i, post) => {
-    // // Scrap URL
-    link = findElement(post, [
-      ".job-content-top-title a",
-      ".job-content-top-title-ltr a",
-      ".job-content-top-title-highlight a",
-    ]).attr("href");
-    link = `https://www.alljobs.co.il${link}`;
-
-    // Create ID
-    id = "id-aj-" + link.slice(link.indexOf("=") + 1);
-
-    // Scrap title
-    title = findElement(post, [
-      ".job-content-top-title h3",
-      ".job-content-top-title-ltr h3",
-      ".job-content-top-title-highlight",
-    ]).text();
-    title = title.replace(/\s+/g, " ");
-
-    // // Scrap description & requirements
-    postJobInfo = findElement(post, [
-      ".job-content-top-desc.AR",
-      ".job-content-top-desc.AL",
-    ]).html();
-
-    if (postJobInfo.includes("דרישות:")) {
-      description = postJobInfo.slice(0, postJobInfo.indexOf("דרישות:"));
-      requirements = postJobInfo.slice(
-        postJobInfo.indexOf("דרישות:") + "דרישות:".length
-      );
-    } else {
-      description = postJobInfo.slice(0, postJobInfo.indexOf("Requirements:"));
-      requirements = postJobInfo.slice(
-        postJobInfo.indexOf("Requirements:") + "Requirements:".length
-      );
-    }
-    // requirements = requirements.slice(postJobInfo.indexOf("Requirements:"));
-    // requirements = "";
-
-    // // Scrap publish time
-    publishTime = findElement(post, [".job-content-top-date"]).text().trim();
-
-    // // Scrap type
-    type = findElement(post, [
-      ".job-content-top-type",
-      ".job-content-top-type-ltr",
-    ])
-      .text()
-      .replace(/\s+/g, " ")
-      .replace("סוג משרה:", "")
-      .replace("Job Type:", "")
-      .trim();
-
-    // // Scrap location
-    let location = [];
-    // // 1) Get locations list
-    locationsEl = findElement(post, [
-      ".job-content-top-location a",
-      ".job-content-top-location-ltr a",
-    ]);
-    // // 2) Push loctaions
-    if (locationsEl)
-      locationsEl.each(function () {
-        location.push($(this).text());
+    // Helper
+    const findElement = function (parentEl, queries) {
+      let childEl;
+      let helper;
+      queries.forEach((query) => {
+        if (!helper) {
+          childEl = $(parentEl).find(query);
+          helper = $(parentEl).find(query).text();
+        }
       });
+      return childEl;
+    };
 
-    jobs_data.push({
-      id,
-      title,
-      link,
-      description,
-      requirements,
-      publishTime,
-      type,
-      location,
+    jobPosts.each((i, post) => {
+      // // Scrap URL
+      link = findElement(post, [
+        ".job-content-top-title a",
+        ".job-content-top-title-ltr a",
+        ".job-content-top-title-highlight a",
+      ]).attr("href");
+      link = `https://www.alljobs.co.il${link}`;
+
+      // Create ID
+      id = "id-aj-" + link.slice(link.indexOf("=") + 1);
+
+      // Scrap title
+      title = findElement(post, [
+        ".job-content-top-title h3",
+        ".job-content-top-title-ltr h3",
+        ".job-content-top-title-highlight",
+      ]).text();
+      title = title.replace(/\s+/g, " ");
+
+      // // Scrap description & requirements
+      postJobInfo = findElement(post, [
+        ".job-content-top-desc.AR",
+        ".job-content-top-desc.AL",
+      ]).html();
+
+      if (postJobInfo.includes("דרישות:")) {
+        description = postJobInfo.slice(0, postJobInfo.indexOf("דרישות:"));
+        requirements = postJobInfo.slice(
+          postJobInfo.indexOf("דרישות:") + "דרישות:".length
+        );
+      } else {
+        description = postJobInfo.slice(
+          0,
+          postJobInfo.indexOf("Requirements:")
+        );
+        requirements = postJobInfo.slice(
+          postJobInfo.indexOf("Requirements:") + "Requirements:".length
+        );
+      }
+      // requirements = requirements.slice(postJobInfo.indexOf("Requirements:"));
+      // requirements = "";
+
+      // // Scrap publish time
+      publishTime = findElement(post, [".job-content-top-date"]).text().trim();
+
+      // // Scrap type
+      type = findElement(post, [
+        ".job-content-top-type",
+        ".job-content-top-type-ltr",
+      ])
+        .text()
+        .replace(/\s+/g, " ")
+        .replace("סוג משרה:", "")
+        .replace("Job Type:", "")
+        .trim();
+
+      // // Scrap location
+      let location = [];
+      // // 1) Get locations list
+      locationsEl = findElement(post, [
+        ".job-content-top-location a",
+        ".job-content-top-location-ltr a",
+      ]);
+      // // 2) Push loctaions
+      if (locationsEl)
+        locationsEl.each(function () {
+          location.push($(this).text());
+        });
+
+      jobs_data.push({
+        id,
+        title,
+        link,
+        description,
+        requirements,
+        publishTime,
+        type,
+        location,
+      });
     });
-  });
 
-  return jobs_data;
+    return jobs_data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 const getJobMaster = async function (query, page = 1) {
-  const url = `https://www.jobmaster.co.il/jobs/?currPage=${page}&q=${query}`;
-  console.log("Start scraping JobMaster...");
-  const html = await axios(url);
-  const $ = cheerio.load(html.data);
-  const jobPosts = $(".ul_results_list .JobItem");
+  try {
+    const url = `https://www.jobmaster.co.il/jobs/?currPage=${page}&q=${query}`;
+    console.log("Start scraping JobMaster...");
+    const html = await axios(url);
+    const $ = cheerio.load(html.data);
+    const jobPosts = $(".ul_results_list .JobItem");
 
-  // Helper
-  const findElement = function (parentEl, queries) {
-    let childEl;
-    let helper;
-    queries.forEach((query) => {
-      if (!helper) {
-        childEl = $(parentEl).find(query);
-        helper = $(parentEl).find(query).text();
+    // Helper
+    const findElement = function (parentEl, queries) {
+      let childEl;
+      let helper;
+      queries.forEach((query) => {
+        if (!helper) {
+          childEl = $(parentEl).find(query);
+          helper = $(parentEl).find(query).text();
+        }
+      });
+      return childEl;
+    };
+
+    const getPostInfo = async function (link) {
+      try {
+        const postHtml = await axios(link);
+        const post$ = cheerio.load(postHtml.data);
+        const jobPost = post$(".JobItem");
+
+        description = findElement(jobPost, [".jobDescription"])
+          .children()
+          .eq(1)
+          .html();
+        requirements = findElement(jobPost, [".jobRequirements "])
+          .children()
+          .eq(1)
+          .html();
+
+        return { description, requirements, link };
+      } catch (err) {
+        console.error(err);
+        throw err;
+      }
+    };
+
+    const jobDataPromises = jobPosts.map(async (i, post) => {
+      try {
+        // Scrap URL
+        link = findElement(post, [".CardHeader"]).attr("href");
+        link = `https://www.jobmaster.co.il/${link}`;
+        // Get post info
+        const postJobInfo = await getPostInfo(link);
+        link = postJobInfo.link;
+
+        // Create ID
+        id = "id-jm-" + link.slice(link.indexOf("=") + 1);
+
+        // Scrap title
+        title = findElement(post, [".CardHeader"]).text();
+
+        // Scrap description
+        description = postJobInfo.description;
+
+        // Scrap requirements
+        requirements = postJobInfo.requirements;
+
+        // Scrap publish time
+        publishTime = findElement(post, [".paddingTop10px .Gray"])
+          .text()
+          .trim();
+
+        // Scrap type
+        type = findElement(post, [".JobExtraInfo .jobType"]).text();
+
+        // Scrap location
+        location = [
+          findElement(post, [".JobExtraInfo .jobLocation"]).text().trim(),
+        ];
+
+        return {
+          id,
+          title,
+          link,
+          description,
+          requirements,
+          publishTime,
+          type,
+          location,
+        };
+      } catch (err) {
+        console.error(err);
+        throw err;
       }
     });
-    return childEl;
-  };
 
-  const getPostInfo = async function (link) {
-    const postHtml = await axios(link);
-    const post$ = cheerio.load(postHtml.data);
-    const jobPost = post$(".JobItem");
+    // Wait for all promises to resolve
+    const jobsData = await Promise.all(jobDataPromises);
 
-    description = findElement(jobPost, [".jobDescription"])
-      .children()
-      .eq(1)
-      .html();
-    requirements = findElement(jobPost, [".jobRequirements "])
-      .children()
-      .eq(1)
-      .html();
-
-    return { description, requirements, link };
-  };
-
-  const jobDataPromises = jobPosts.map(async (i, post) => {
-    // Scrap URL
-    link = findElement(post, [".CardHeader"]).attr("href");
-    link = `https://www.jobmaster.co.il/${link}`;
-    // Get post info
-    const postJobInfo = await getPostInfo(link);
-    link = postJobInfo.link;
-
-    // Create ID
-    id = "id-jm-" + link.slice(link.indexOf("=") + 1);
-
-    // Scrap title
-    title = findElement(post, [".CardHeader"]).text();
-
-    // Scrap description
-    description = postJobInfo.description;
-
-    // Scrap requirements
-    requirements = postJobInfo.requirements;
-
-    // Scrap publish time
-    publishTime = findElement(post, [".paddingTop10px .Gray"]).text().trim();
-
-    // Scrap type
-    type = findElement(post, [".JobExtraInfo .jobType"]).text();
-
-    // Scrap location
-    location = [
-      findElement(post, [".JobExtraInfo .jobLocation"]).text().trim(),
-    ];
-
-    return {
-      id,
-      title,
-      link,
-      description,
-      requirements,
-      publishTime,
-      type,
-      location,
-    };
-  });
-
-  // Wait for all promises to resolve
-  const jobsData = await Promise.all(jobDataPromises);
-
-  return jobsData;
+    return jobsData;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 module.exports = { getAllJobs, getJobMaster, getDrushim };
